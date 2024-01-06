1083d83403785de4123dbefb9001b1c1be193b08
miladlotfi <miladlotfi66@gmail.com>
2024-01-06 17:33:03 +0330

    make delete api and buttom

--- components/module/Card.js
+++ components/module/Card.js
@@ -1,17 +1,27 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-function Card( {customer} ) {
-    // console.log(customer._id);
-const deleteHandler=()=>{
-    
-}
+function Card({ customer }) {
+    const router=useRouter();
+
+  // console.log(customer._id);
+  const deleteHandler = async () => {
+    const res = await fetch(`/api/delete/${customer._id}`, {
+      method: "DELETE",
+    });
+    const data=await res.json();
+    console.log(data);
+    if(data.status==="success"){
+        router.reload();
+    }
+  };
   return (
     <div className="card">
       <div className="card__details">
         <p>
           {customer.name} {customer.lastName}
-        </p>y
-        <p>{customer.email}</p>
+        </p>
+        y<p>{customer.email}</p>
         <div className="card__buttons">
           <button onClick={deleteHandler}>delete</button>
           <Link href={`/edit/${customer._id}`}>edit</Link>
---
+++ pages/api/delete/[customerId].js
@@ -0,0 +1,22 @@
+import connectDb from "../../../utils/connectDB";
+import Customer from "../../../models/Customer";
+
+export default async function Handler(req, res) {
+  try {
+    await connectDb();
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({status:"failed",message:"conect to db failed"})
+  }
+  if (req.method === "DELETE") {
+    const id=req.query.customerId;
+try {
+    await Customer.deleteOne({_id:id})
+    res.status(200).json({status:"success",message:"delete customer success"})
+} catch (err) {
+    console.log(err.message);
+    res.status(200).json({status:"success",message:"delete customer success"})
+    
+}
+  }
+}
